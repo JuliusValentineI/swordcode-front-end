f53d09921037152f020c085b3b19a02c8d89973a
JuliusValentineI <hernanigonzalez.personal@gmail.com>
2024-03-10 10:39:18 -0500

    Front: Create [slug].tsx in blog folder and header.tsx component

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -7,39 +7,39 @@ import ReactMarkdown from "react-markdown";
 const apiURL = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Page() {
-  const router = useRouter();
-  let [content, setContent] = useState(``);
-  let [filename, setFilename] = useState("");
+	const router = useRouter();
+	let [content, setContent] = useState(``);
+	let [filename, setFilename] = useState(``);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getFile(
-          `${apiURL}/api/v1/showFile/?article=${router.query.slug}`,
-        );
-        setContent(`${response.content}`);
-        setFilename(response.filename);
-      } catch (e) {
-        console.log(e);
-      }
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await getFile(
+					`${apiURL}/api/v1/showFile/?article=${router.query.slug}`,
+				);
+				setContent(`${response.content}`);
+				setFilename(response.filename);
+			} catch (e) {
+				console.log(e);
+			}
+		};
 
-    fetchData();
-  }, [router.query.slug]);
+		fetchData();
+	}, [router.query.slug]);
 
-  return (
-    <>
+	return (
+		<>
 			<Header />
 			<div className="container">
-      {content ? (
-        <div>
-          <h1>{filename}</h1>
-          <ReactMarkdown>{content}</ReactMarkdown>
-        </div>
-      ) : (
-        <p>-- No esta disponible --</p>
-      )}
+				{content ? (
+					<div>
+						<h1>{filename}</h1>
+						<ReactMarkdown>{content}</ReactMarkdown>
+					</div>
+				) : (
+					<p>-- No esta disponible --</p>
+				)}
 			</div>
-    </>
-  );
+		</>
+	);
 }
